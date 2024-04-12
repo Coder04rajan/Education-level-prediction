e5462ddf39209b8f309545f8cec35aaa345b9279
Coder04rajan <118625483+Coder04rajan@users.noreply.github.com>
2024-04-13 04:55:44 +0530

    Update validation.py

--- validation.py
+++ validation.py
@@ -50,11 +50,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.7, random_
 knn = KNeighborsClassifier() #initialising the knn classifier
 # Define the parameter grid for GridSearchCV to get the accurate hyperparameters
 param_grid = {
-    'n_neighbors': [19,20,21],
+    'n_neighbors': [10,20,30],
     'weights': ['uniform', 'distance'],
     'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
     'leaf_size': [10, 20, 30],
-    'p': [1,2,3,4]  # p=1 for Manhattan distance, p=2 for Euclidean distance
+    'p': [1,2]  # p=1 for Manhattan distance, p=2 for Euclidean distance
 }
 
 # Initialize GridSearchCV
